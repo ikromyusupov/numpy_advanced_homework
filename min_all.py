bf6e7364282e1789fee3538088e95ffa5633e862
Ikrom <ikromyusupov09@gmail.com>
2022-06-07 00:17:16 +0500

    the min of all numbers in the array

--- min_all.py
+++ min_all.py
@@ -8,3 +8,8 @@ def min_all(arr: np.ndarray) -> int:
     Returns:
         int: min of all numbers
     """
+    min = arr[0]
+    for k in range(1,len(arr)):
+        if arr[k] < min:
+            min = arr[k]
+    return min
